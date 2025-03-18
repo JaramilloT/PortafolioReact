892d6536060efb8f67f1e33c003cc51332b54250
JaramilloT <jaramillotamayosantiago@gmail.com>
2025-03-18 15:52:52 -0500

    Probando las medias querys

--- src/Components/Layouts/Footer/Footer.jsx
+++ src/Components/Layouts/Footer/Footer.jsx
@@ -6,11 +6,13 @@ export const Footer = () => {
   return (
     <>
     <footer>
-    <ul>
-      <li><a href="https://facebook.com" target="_blank">Facebook</a></li>
-      <li><a href="https://twitter.com" target="_blank">Twitter</a></li>
-      <li><a href="https://instagram.com" target="_blank">Instagram</a></li>
-    </ul>
+      <div className='containerFooter'>
+        <ul>
+          <li><a href="https://facebook.com" target="_blank">Facebook</a></li>
+          <li><a href="https://twitter.com" target="_blank">Twitter</a></li>
+          <li><a href="https://instagram.com" target="_blank">Instagram</a></li>
+        </ul>
+      </div>
   </footer>
     </>
   )
