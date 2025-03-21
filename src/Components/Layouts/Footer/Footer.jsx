1575fd160fc8115421759f9598c7c0f90cd0b222
JaramilloT <jaramillotamayosantiago@gmail.com>
2025-03-20 21:52:37 -0500

    Responsivo casi terminado para movil

--- src/Components/Layouts/Footer/Footer.jsx
+++ src/Components/Layouts/Footer/Footer.jsx
@@ -9,7 +9,7 @@ export const Footer = () => {
       <div className='containerFooter'>
         <ul>
           <li><a href="https://facebook.com" target="_blank">Facebook</a></li>
-          <li><a href="https://twitter.com" target="_blank">Twitter</a></li>
+          <li><a href="https://github.com/JaramilloT" target="_blank">GitHub</a></li>
           <li><a href="https://instagram.com" target="_blank">Instagram</a></li>
         </ul>
       </div>
